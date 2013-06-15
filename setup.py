4ef43eec9a6c5000ca068660d3e7ae86b3f823ae
Adam Spence <aspence1@gmail.com>
2013-06-15 23:11:38 +0100

    fixing a few things in setp

--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
+import codecs
+import os
 from setuptools import setup, find_packages
+
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='django_testdriver',
-    version='0.1.2',
-	description='A custom JsTestDriver spec runner etc.',
+    version=__import__('django_testdriver').__version__,
+    description='A custom JsTestDriver spec runner etc.',
     author='spenoir',
     author_email='aspence1@gmail.com',
     packages=find_packages(),
     url='http://github.com/spenoir/django_testdriver',
 
-    long_description=open('README.md').read(),
+    long_description=read('README.md'),
     install_requires=[
         'pyyaml',
-        'django-nose',
-        'milkman',
-        'mock',
     ],
-    classifiers = [
+    classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License', # example license
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     include_package_data=True,
 )
